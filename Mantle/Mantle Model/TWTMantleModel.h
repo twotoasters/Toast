b6f51ed934ea53dd57ae11a4513098a7503db597
Prachi Gauriar <prachi@twotoasters.com>
2014-03-09 16:22:43 -0400

    Implement base Mantle model extension that handles inverse relationships

---
+++ Mantle/Mantle Model/TWTMantleModel.h
@@ -0,0 +1,46 @@
+//
+//  TWTMantleModel.h
+//  Toast
+//
+//  Created by Prachi Gauriar on 3/8/2014.
+//  Copyright (c) 2014 Two Toasters, LLC.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+#import <Mantle/Mantle.h>
+
+@interface TWTMantleModel : MTLModel
+
+/*!
+ @abstract Returns a set of property keys that the receiver’s instances use to represent inverse relationships.
+ @discussion Mantle can’t detect circular references, and thus its default implementations of -description,
+     -hash, and -isEqual: will recurse infinitely unless inverse relationships are excluded. By implementing
+     this method and returning any inverse relationships for class’s inverse relationships, the aforementioned
+     methods will just work. The default implementation returns the empty set.
+
+     Subclasses should take care that their implementation of this method returns their inverse relationship
+     property keys and their superclass’s. A typical implementation looks like this:
+ 
+         return [[super inverseRelationshipPropertyKeys] setByAddingObjectsFromArray:@[ o1, o2, …, oN ]];
+ @result A set of property keys that the receiver’s instances use for inverse relationships.
+ */
++ (NSSet *)inverseRelationshipPropertyKeys;
+
+@end
--- Podfile
+++ Podfile
@@ -1,5 +1,7 @@
 platform :ios, '7.0'
 
+pod 'Mantle', '1.3.1'
+
 target "ToastTests" do
   pod 'URLMock/TestHelpers', '~> 1.0.1'
 end
