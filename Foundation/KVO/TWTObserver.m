f63f87e80b8852842e9fc4656370b36d3e0a2a9c
Josh Johnson <josh@twotoasters.com>
2014-03-24 10:28:35 -0400

    added observer method to more easily encapsulates KVO

---
+++ Foundation/KVO/TWTObserver.m
@@ -0,0 +1,187 @@
+//
+//  TWTObserver.m
+//  Toast
+//
+//  Created by Josh Johnson on 3/12/14.
+//  Copyright (c) 2014 Two Toasters.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+#import "TWTObserver.h"
+#import "NSMethodSignature+TWTToast.h"
+
+@interface TWTObserver ()
+
+@property (nonatomic, weak) id object;
+@property (nonatomic, copy) NSString *keyPath;
+
+@property (nonatomic, weak) id target;
+@property (nonatomic, assign) SEL action;
+@property (nonatomic, copy) TWTObserverChangeBlock changeBlock;
+
+@end
+
+@implementation TWTObserver
+
+#pragma mark - TWTObserver
+
++ (instancetype)observerWithObject:(id)object keyPath:(NSString *)keyPath changeBlock:(TWTObserverChangeBlock)changeBlock
+{
+    return [[self alloc] initWithObject:object keyPath:keyPath changeBlock:changeBlock];
+}
+
+
++ (instancetype)observerWithObject:(id)object keyPath:(NSString *)keyPath target:(id)target action:(SEL)action
+{
+    return [[self alloc] initWithObject:object keyPath:keyPath target:target action:action];
+}
+
+
+- (instancetype)initWithObject:(id)object keyPath:(NSString *)keyPath changeBlock:(TWTObserverChangeBlock)changeBlock
+{
+    self = [super init];
+    if (self) {
+        self.object = object;
+        self.keyPath = keyPath;
+        self.changeBlock = changeBlock;
+        
+        [object addObserver:self
+                 forKeyPath:keyPath
+                    options: NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld 
+                    context:(__bridge void *)self];
+    }
+    return self;
+}
+
+
+- (instancetype)initWithObject:(id)object keyPath:(NSString *)keyPath target:(id)target action:(SEL)action
+{
+    self = [super init];
+    if (self) {
+        self.object = object;
+        self.keyPath = keyPath;
+        self.target = target;
+        self.action = action;
+        
+        if (![self target:target hasValidSignatureForSelector:action]) {
+            @throw [NSException exceptionWithName:NSInvalidArgumentException
+                                           reason:@"Action Method must conform to -(void)object:(id)object changeDictionary:(NSDictionary *)changeDictionary;"
+                                         userInfo:nil];
+            return nil;
+        }
+        
+        [object addObserver:self
+                 forKeyPath:keyPath
+                    options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld
+                    context:(__bridge void *)self];
+    }
+    return self;
+}
+
+
+- (void)dealloc
+{
+    [_object removeObserver:self forKeyPath:_keyPath context:(__bridge void *)self];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+    if (context == (__bridge void *)self) {
+        if (self.changeBlock) {
+            self.changeBlock(object, change);
+        }
+        else if ([self.target respondsToSelector:self.action]) {
+            NSMethodSignature *methodSignature = [self.target methodSignatureForSelector:self.action];
+            NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:methodSignature];
+            invocation.target = self.target;
+            invocation.selector = self.action;
+            
+            if (methodSignature.numberOfArguments > 2) {
+                // add object argument to at index 2
+                [invocation setArgument:&object atIndex:2];
+            }
+            
+            if (methodSignature.numberOfArguments > 3) {
+                // add change dictionary at index 3
+                [invocation setArgument:&change atIndex:3];
+            }
+            
+            [invocation invoke];
+        }
+    }
+    else {
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
+}
+
+
+#pragma mark - Model Method Signatures for verifying action
+
+- (void)model_objectChanged
+{
+    // Empty Implementation, method is only used for verifying action method signature
+}
+
+
+- (void)model_objectChanged:(id)object
+{
+    // Empty Implementation, method is only used for verifying action method signature
+}
+
+
+- (void)model_objectChanged:(id)object changeDictionary:(NSDictionary *)changeDictionary
+{
+    // Empty Implementation, method is only used for verifying action method signature
+}
+
+
+#pragma mark - Validation
+
+- (BOOL)target:(id)target hasValidSignatureForSelector:(SEL)action;
+{
+    if (self.action == NULL) {
+        return NO;
+    }
+    
+    NSMethodSignature *actionMethodSignature = [self.target methodSignatureForSelector:self.action];
+    NSArray *validMethodSignatures = @[ [self methodSignatureForSelector:@selector(model_objectChanged)],
+                                        [self methodSignatureForSelector:@selector(model_objectChanged:)],
+                                        [self methodSignatureForSelector:@selector(model_objectChanged:changeDictionary:)] ];
+    
+    return [self methodSignature:actionMethodSignature matchesMethodSignatures:validMethodSignatures];
+}
+
+
+- (BOOL)methodSignature:(NSMethodSignature *)methodSignature matchesMethodSignatures:(NSArray *)methodSignatures
+{
+    BOOL validMethodSignature = NO;
+    
+    for (NSMethodSignature *modelMethodSignature in methodSignatures) {
+        if ([methodSignature twt_isEqualToMethodSignature:modelMethodSignature]) {
+            validMethodSignature = YES;
+            break;
+        }
+    }
+    
+    return validMethodSignature;
+}
+
+
+@end
---
+++ ToastTests/Foundation/KVO/TWTObserverTests.m
@@ -0,0 +1,114 @@
+//
+//  TWTObserverTests.m
+//  Toast
+//
+//  Created by Josh Johnson on 3/23/2014.
+//  Copyright (c) 2014 Two Toasters.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+#import <XCTest/XCTest.h>
+#import <URLMock/UMKTestUtilities.h>
+#import "TWTRandomizedTestCase.h"
+#import "TWTObserver.h"
+
+@interface TWTSampleObservableObject : NSObject
+@property (nonatomic, copy) NSString *targetValue;
+@property (nonatomic, copy) NSString *sampleProperty;
+@end
+
+@implementation TWTSampleObservableObject
+@end
+
+@interface TWTObserverTests : TWTRandomizedTestCase
+
+@end
+
+@implementation TWTObserverTests
+
+- (void)testObserverWithBlockBasedChange
+{
+    NSString *oldValue = UMKRandomUnicodeString();
+    NSString *newValue = UMKRandomUnicodeString();
+    
+    TWTSampleObservableObject *object = [[TWTSampleObservableObject alloc] init];
+    object.sampleProperty = oldValue;
+    
+    __unused TWTObserver *testObserver = [TWTObserver observerWithObject:object keyPath:NSStringFromSelector(@selector(sampleProperty)) changeBlock:^(id changingObject, NSDictionary *changeDictionary) {
+        XCTAssertEqual(changingObject, object, @"Observed Objects are not the same");
+        XCTAssertNotNil(changeDictionary, @"Change dictionary should be presented");
+        XCTAssertEqual(newValue, changeDictionary[ NSKeyValueChangeNewKey ], @"Change Dictionary values are not equal");
+    }];
+    
+    object.sampleProperty = newValue;
+}
+
+
+- (void)testObserverWithMethodBasedChange_zeroArgumentForm
+{
+    [self performTestWithAction:@selector(objectChanged)];
+}
+
+
+- (void)testObserverWithMethodBasedChange_oneArgumentForm
+{
+    [self performTestWithAction:@selector(objectChanged:)];
+}
+
+
+- (void)testObserverWithMethodBasedChange_twoArgumentForm
+{
+    [self performTestWithAction:@selector(objectChanged:changes:)];
+}
+
+- (void)performTestWithAction:(SEL)action
+{
+    TWTSampleObservableObject *object = [[TWTSampleObservableObject alloc] init];
+    object.targetValue = UMKRandomUnicodeString();
+    object.sampleProperty = UMKRandomUnicodeString();
+    
+    __unused TWTObserver *testObserver = [TWTObserver observerWithObject:object keyPath:NSStringFromSelector(@selector(sampleProperty)) target:self action:action];
+    
+    object.sampleProperty = object.targetValue;
+}
+
+
+#pragma mark - Observer Actions
+
+- (void)objectChanged
+{
+    XCTAssertTrue(YES, @"Nothing to verify, must be called.");
+}
+
+
+- (void)objectChanged:(TWTSampleObservableObject *)object
+{
+    XCTAssertEqual([object class], [TWTSampleObservableObject class], @"Observed Objects are not the same");
+}
+
+
+- (void)objectChanged:(TWTSampleObservableObject *)object changes:(NSDictionary *)changes
+{
+    XCTAssertEqual([object class], [TWTSampleObservableObject class], @"Observed Objects are not the same");
+    XCTAssertEqual(object.targetValue, changes[ NSKeyValueChangeNewKey ], @"Change Dictionary values are not equal");
+}
+
+
+@end
