ac6767e05d2681a7b71251ec4ee8399afda5e9ba
Dfowj <duncan@twotoasters.com>
2014-09-18 16:31:01 -0400

    Added UIButtons with block tap handlers

---
+++ UIKit/Blocks/UIButton+TWTBlocks.m
@@ -0,0 +1,41 @@
+//
+//  UIButton+TWTBlocks.m
+//  Toast
+//
+//  Created by Duncan Lewis on 8/1/14.
+//  Copyright (c) 2014 Two Toasters, LLC. All rights reserved.
+//
+
+#import "UIButton+TWTBlocks.h"
+#import <objc/runtime.h>
+
+static char kTapHandlerKey;
+
+@implementation UIButton (TWTBlocks)
+
+- (TWTButtonTapBlock)twt_tapHandler
+{
+    return objc_getAssociatedObject(self, &kTapHandlerKey);
+}
+
+- (void)setTwt_tapHandler:(TWTButtonTapBlock)twt_tapHandler
+{
+    // remove old tap handler
+    void (^oldTapHandler)(UIButton *) = objc_getAssociatedObject(self, &kTapHandlerKey);
+    if (oldTapHandler) {
+        [self removeTarget:self action:@selector(buttonWasTapped:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    objc_setAssociatedObject(self, &kTapHandlerKey, twt_tapHandler, OBJC_ASSOCIATION_COPY_NONATOMIC);
+    if (twt_tapHandler) {
+        [self addTarget:self action:@selector(buttonWasTapped:) forControlEvents:UIControlEventTouchUpInside];
+    }
+}
+
+- (void)buttonWasTapped:(id)sender
+{
+    if (self.twt_tapHandler) {
+        self.twt_tapHandler(sender);
+    }
+}
+
+@end
