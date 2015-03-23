a293e295269c8b61c7bfb4b7ff7eea0b34b2efcd
Josh Johnson <josh@twotoasters.com>
2015-03-23 16:04:06 -0400

    added randomized tests for flatten dictionary and flatten collection

--- ToastTests/Foundation/Block Enumeration/TWTBlockEnumerationTests.m
+++ ToastTests/Foundation/Block Enumeration/TWTBlockEnumerationTests.m
@@ -105,21 +105,22 @@
 
 - (void)testDictionaryBlockEnumerationFlatten
 {
-    NSDictionary *groups = @{ @"android" : @[ @"Fred", @"James", @"Marcelle", @"Curtis", @"Chris", @"Patrick", @"Ben" ],
-                              @"ios" : @[ @"Andrew", @"Prachi", @"Ameir", @"Kevin", @"Jill", @"Duncan", @"Tom", @"Steve"],
-                              @"mgmt" : @{ @"development" : @[ @"Josh" ] },
-                              @"ceo" : @"Rachit" };
-
-    NSDictionary *expected = @{ @"android" : @[ @"Fred", @"James", @"Marcelle", @"Curtis", @"Chris", @"Patrick", @"Ben" ],
-                                @"ios" : @[ @"Andrew", @"Prachi", @"Ameir", @"Kevin", @"Jill", @"Duncan", @"Tom", @"Steve"],
-                                @"development" : @[ @"Josh" ] ,
-                                @"ceo" : @"Rachit" };
-
-    NSArray *flattenedGroups = [groups twt_flatten];
+    NSDictionary *randomStringDictionary = [self randomStringDictionary];
+    NSDictionary *randomSubDictionary = @{ UMKRandomAlphanumericString() : [self randomNumberArray] };
     
-    XCTAssertNotNil(flattenedGroups, @"Returned collection is nil");
-    XCTAssertEqual([expected count], [flattenedGroups count], @"Returned collection does not match size of original collection");
-    XCTAssertEqualObjects(expected, flattenedGroups, @"Flattened collection does not match the expected collection");
+    NSMutableDictionary *randomDictionary = [[NSMutableDictionary alloc] init];
+    [randomDictionary addEntriesFromDictionary:randomStringDictionary];
+    [randomDictionary setObject:randomSubDictionary forKey:UMKRandomAlphanumericString()];
+    
+    NSMutableDictionary *expectedDictionary = [[NSMutableDictionary alloc] init];
+    [expectedDictionary addEntriesFromDictionary:randomStringDictionary];
+    [expectedDictionary addEntriesFromDictionary:randomSubDictionary];
+
+    NSDictionary *flattenedDictionary = [randomDictionary twt_flatten];
+    
+    XCTAssertNotNil(flattenedDictionary, @"Returned collection is nil");
+    XCTAssertEqual([expectedDictionary count], [flattenedDictionary count], @"Returned collection does not match size of original collection");
+    XCTAssertEqualObjects(expectedDictionary, flattenedDictionary, @"Flattened collection does not match the expected collection");
 }
 
 
@@ -258,17 +259,22 @@
 
 - (void)testCollectionBlockEnumerationFlatten
 {
-    NSArray *groups = @[ @[ @[ @"Josh" ] ],
-                         @[ @"Tom" ],
-                         @[ @"Jill" ],
-                         @[ @"Ameir", @"Kevin", @"Steve" ],
-                         @[ @"Andrew", @"Duncan", @"Prachi" ] ];
-    NSArray *groupsExpected = @[ @"Josh", @"Tom", @"Jill", @"Ameir", @"Kevin", @"Steve", @"Andrew", @"Duncan", @"Prachi" ];
-    NSArray *flattenedGroups = [groups twt_flatten];
+    NSMutableArray *randomArray = [[NSMutableArray alloc] init];
 
-    XCTAssertNotNil(flattenedGroups, @"Returned collection is nil");
-    XCTAssertEqual([groupsExpected count], [flattenedGroups count], @"Returned collection does not match size of original collection");
-    XCTAssertEqualObjects(groupsExpected, flattenedGroups, @"Flattened collection does not match the expected collection");
+    for (NSUInteger i = 0; i < [self randomCount]; ++i) {
+        [randomArray addObject:[self randomStringArray]];
+    }
+    
+    NSMutableArray *expectedArray = [[NSMutableArray alloc] init];
+    for (NSArray *randomStringArray in randomArray) {
+        [expectedArray addObjectsFromArray:randomStringArray];
+    }
+
+    NSArray *flattenedArray = [randomArray twt_flatten];
+    
+    XCTAssertNotNil(flattenedArray, @"Returned collection is nil");
+    XCTAssertEqual([expectedArray count], [flattenedArray count], @"Returned collection does not match size of original collection");
+    XCTAssertEqualObjects(expectedArray, flattenedArray, @"Flattened collection does not match the expected collection");
 }
 
 
