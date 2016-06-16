1a873162594ff34b6a6c49de06c176cf9a2cad5f
Dfowj <duncan@twotoasters.com>
2016-06-16 11:05:16 -0400

    Removed redundant color initialization method, added license to TWTGradient files

--- UIKit/Gradient/TWTGradient.h
+++ UIKit/Gradient/TWTGradient.h
@@ -4,6 +4,24 @@
 //  Created by Prachi Gauriar on 11/17/2015.
 //  Copyright © 2015 Ticketmaster Entertainment, Inc. All rights reserved.
 //
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
 
 @import UIKit;
 
@@ -89,15 +107,6 @@ NS_ASSUME_NONNULL_BEGIN
  @result A newly initialized TWTGradientColor instance.
  */
 - (instancetype)initWithColor:(UIColor *)color location:(CGFloat)location NS_DESIGNATED_INITIALIZER;
-
-/*!
- @abstract Convenience initializer that initializes a new gradient color with a hexadecimal RGB value.
- @discussion The color is assumed to have an alpha component of 1.0.
- @param hex The new instance’s color as a hexadecimal RGB value in 0xRRGGBB format.
- @param location The new instance’s location. Values of this parameter are pinned to be between 0.0 and 1.0, inclusive.
- @result A newly created TWTGradientColor instance with the specified color and location.
- */
-- (instancetype)initWithHexadecimalRGB:(NSUInteger)hex location:(CGFloat)location;
 
 @end
 
--- UIKit/Gradient/TWTGradient.m
+++ UIKit/Gradient/TWTGradient.m
@@ -4,6 +4,24 @@
 //  Created by Prachi Gauriar on 11/17/2015.
 //  Copyright © 2015 Ticketmaster Entertainment, Inc. All rights reserved.
 //
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
 
 #import "TWTGradient.h"
 
@@ -140,16 +158,6 @@
     }
 
     return self;
-}
-
-
-- (instancetype)initWithHexadecimalRGB:(NSUInteger)hex location:(CGFloat)location
-{
-    CGFloat red = (CGFloat)((hex & 0xff0000) >> 16) / 255.0;
-    CGFloat green = (CGFloat)((hex & 0x00ff00) >> 8) / 255.0;
-    CGFloat blue = (CGFloat)(hex & 0x0000ff) / 255.0;
-    UIColor *color = [UIColor colorWithRed:red green:green blue:blue alpha:1.0];
-    return [self initWithColor:color location:location];
 }
 
 
